91a377f219084790809c4c40782311077a8d3cc6
ith3 <ith3@pitt.edu>
2016-12-08 15:26:12 -0500

    Assignment Three

--- f2016_cs8_ith3_fp/f2016_cs8_ith3_fp.py
+++ f2016_cs8_ith3_fp/f2016_cs8_ith3_fp.py
@@ -14,41 +14,51 @@
 #
 #######################################################
 
-
+# This class creats a participant object as requested
 class participant():
-    name = ""
+    # Initializing variables
+    name = "Unknown"
     runs = 0
     dist = 0
 
+    # Methods (Pretty self explanatory by name)
     def __str__(self):
         return "Name : " + formatItem(self.name) + ". Distance run : " + formatItem(self.dist) + ". Runs : " + formatItem(self.runs)
 
     def __init__(self, name, dist = 0):
         self.name = name
         self.dist = dist
-        self.runs = 1
+        if(dist == 0):
+            self.runs = 0
+        else:
+            self.runs = 1
 
     def getName(self):
         return self.name
+
     def getDistance(self):
         return self.dist
+
     def getRuns(self):
         return self.runs
+
     def addDistance(self,newDist):
         self.dist += newDist
         self.runs += 1
+
     def addDistances(self, dists):
         for i in len(dists):
             self.dist += dists[i]
             self.runs += 1
-# End parts
 
+    def outputPrint(self):
+        return self.name + "," + str(self.runs) + "," + str(self.dist) + "\n"
+# End partticipant()
 
-##### assignment 3 stuff for copy/paste: #####
 
 # This function gets passed a file name and creates a dictionary containing
 # the information inside the file, then returns that dictionary
-def makeDict(fileName): ###KEEPING AS IS
+def makeDict(fileName):
 
     # initializing variables and opening the file
     d = {}
@@ -77,39 +87,9 @@ def makeDict(fileName): ###KEEPING AS IS
     return d, lineNum
 # End makeDict()
 
-# This function takes someones name (as well as some other numbers that help
-# with finding info, and compliles all of the information about them contained
-# in the given files
-def buildProfile(name, numFiles, fileNames): ###ADJUST TO RIP OFF OF AN OBJ
-                                        ### PROBABLY NOT NECESSARY
-    # Initializing variables
-    numRecords = 0
-    totRan = 0
 
-    # Finds if the name is contained in any of the files, if so retrieved
-    # info under that name and returns how many times they appear and total distance ran
-    for i in range(numFiles):
-        dict, lines = makeDict(fileNames[i])
-        if(name in dict):
-            numRecords += 1
-            totRan += dict[name]
-        else:() #    Who cares
-    return numRecords,totRan
-# End buildProfile
-
-# This function is for adding new names to a list, without adding names
-# already contained in said list
-def incorporate(nameList, dict): ### PROBABLY DUMP
-    newNames = []
-    for key in dict:
-        newNames.append(key)
-    newNameList = list(set(nameList + newNames))
-    return newNameList
-# End incorporate()
-
-# This function is for formatting any numbers to be printed
-# Gets a number coming in and returns a nice neat string
-def formatItem(itm):   ### ADD STR
+# This function is for formatting any numbers/str to be printed
+def formatItem(itm):
     if(isinstance(itm,int)):
         return "{:>3}".format(itm)
     elif (isinstance(itm, float)):
@@ -123,7 +103,7 @@ def formatItem(itm):   ### ADD STR
 
 
 def main():
-    ### GOOD BLOCK
+
     # Opens and closes the master file and stores the names of the files to open in fileNames
     opened = False
     while(not opened):
@@ -155,7 +135,7 @@ def main():
                 participants[key] = participant(key,dict[key])
 
 
-    # Initializing statistical variables that will be part of the output
+    # Initializing statistical variables that will be part of the statistical output
     totDist = 0
     maxDist = 0
     maxDistName = ""
@@ -163,12 +143,14 @@ def main():
     minDistName = ""
     partsWMR = 0
 
-    ### CHANGE TO GENERATE FROM LIST OF OBJS INSTEAD OF BUILDPROFILE
+
     # Creates and fills the output file, while creating the requested stats
-    outputFile = open("f2016_cs8_ith3_fp.data.output.csv","w") # use the join method
+    outputFile = open("f2016_cs8_ith3_fp.data.output.csv","w")
     outputFile.writelines("name, number of records, total distance ran\n")
     for part in participants:
-        outputFile.writelines(participants[part].getName() + "," + str(participants[part].getDistance()) + "," + str(participants[part].getRuns()) + "\n")
+        # Write the line in the output file
+        outputFile.writelines(participants[part].outputPrint())
+        # Stat stuff
         totDist += participants[part].getDistance()
         if(participants[part].getDistance() > maxDist):
             maxDist = participants[part].getDistance()
@@ -180,6 +162,7 @@ def main():
         if(participants[part].getRuns() > 1):
             partsWMR += 1
         else: () # Who cares
+    outputFile.close()
 
     # Output
     print("Number of Input files read   : " + formatItem(numFiles))
